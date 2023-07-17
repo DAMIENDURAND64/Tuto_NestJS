284138f5fff159c15be810be46b3dd23cf132e21
DAMIENDURAND64 <damien64durand@gmail.com>
2023-07-17 22:11:33 +0200

    route summary works

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,8 +1,23 @@
+import { ReportType } from 'src/data';
 import { Injectable } from '@nestjs/common';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
 export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
   calculateSummary() {
-    return;
+    const totalIncome = this.reportService
+      .getAllReports(ReportType.INCOME)
+      .reduce((acc, report) => acc + report.amount, 0);
+
+    const totalExpense = this.reportService
+      .getAllReports(ReportType.EXPENSE)
+      .reduce((acc, report) => acc + report.amount, 0);
+
+    return {
+      totalIncome,
+      totalExpense,
+      balance: totalIncome - totalExpense,
+    };
   }
 }
